a00c553b05ba8778468436ea28f80d541aca24e1
ansh <anshshah2508@gmail.com>
2026-06-15 15:39:09 +0530

    Update validating_json_field.js

--- response_body/validating_json_field.js
+++ response_body/validating_json_field.js
@@ -1,1 +1,23 @@
+//validating JSON Fields in Response
+/*THE EXAMPLE STRUCTURE:
+{
+"id":1,
+"name":"john",
+"location":"india",
+"phone":"1234567890",
+"courses":["java", "selenium"]
+}
+*/
 
+//THE TESTING CODE:
+pm.test("Value of location field in India", () =>
+  {
+    var jsonData=pm.response.json();
+    pm.expect(jsonData.id).to.eql(1);
+    pm.expect(jsonData.name).to.eql("john");
+    pm.expect(jsonData.location).to.eql("india");
+    pm.expect(jsonData.phone).to.eql("1234567890");
+    pm.expect(jsonData.courses[0]).to.eql("java");
+    pm.expect(jsonData.courses[1]).to.eql("selenium");
+  }  
+);
