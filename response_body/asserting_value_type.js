92f791ebdb961223e6cd75a34ce0a129650ecf64
ansh <anshshah2508@gmail.com>
2026-06-15 15:21:57 +0530

    Update asserting_value_type.js

--- response_body/asserting_value_type.js
+++ response_body/asserting_value_type.js
@@ -1,1 +1,20 @@
-//test 
+//test the type of any part of the response
+/*THE EXAMPLE STRUCTURE:
+{
+"id":1,
+"name":"john",
+"phone":"1234567890",
+"courses":["java", "selenium"]
+}
+*/
+
+//THE TESTING CODE:
+const jsonData=pm.response.json();
+pm.test("test data type of the response", () =>
+  {
+    pm.expect(jsonData).to.be.an("object");
+    pm.expect(jsonData.name).to.be.an("string");
+    pm.expect(jsonData.id).to.be.an("number");
+    pm.expect(jsonData.courses).to.be.an("array");
+  }
+);        
